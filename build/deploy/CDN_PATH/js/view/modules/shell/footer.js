b60f447f7c63d88ec6bdc53ee1433531efd8d123
simon jullien <simonr.jullien@gmail.com>
2014-03-31 13:44:26 +0200

    initial navigation

--- build/deploy/CDN_PATH/js/view/modules/shell/footer.js
+++ build/deploy/CDN_PATH/js/view/modules/shell/footer.js
@@ -24,9 +24,33 @@ define([
 
     return Backbone.View.extend({
 
+        $privacy:null,
+        $warranty:null,
+
         initialize: function() {
+            _.bindAll(this,"onClickPrivacy","onClickWarranty");
             var allNavigation = AppModel.getNavigationModel().getAllRoutes();
             Utils.compileAndAppendTemplate(Template,{'navigation':allNavigation},this,false);
+
+            this.$privacy = this.$el.find('.privacy-js');
+            this.$warranty = this.$el.find('.warranty-js');
+
+            this.addEvents();
+        },
+
+        addEvents:function(){
+            this.$privacy.on('click', this.onClickPrivacy);
+            this.$warranty.on('click', this.onClickWarranty);
+        },
+
+        onClickPrivacy:function(e){
+            e.preventDefault();
+            Utils.openPopup($(e.currentTarget).attr('href'), 800,600, 'directories=no, scrollbars=yes, titlebar=no,toolbar=no,location=no,status=no,menubar=no,resizable=no');
+        },
+
+        onClickWarranty:function(e){
+            e.preventDefault();
+            Utils.openPopup($(e.currentTarget).attr('href'), 800,600, 'directories=no, scrollbars=yes, titlebar=no,toolbar=no,location=no,status=no,menubar=no,resizable=no');
         },
 
         render: function() {}
